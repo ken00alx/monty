50a486235026a4feca27bc190e71ef14c3ad8064
ken00alx <kenny933@gmail.com>
2023-03-27 14:27:33 -0700

    stack flow

--- monty.h
+++ monty.h
@@ -1,59 +1,24 @@
-#ifndef MONTY_H
-#define MONTY_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include "monty.h"
 
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * mul_top - multiplies the second element of the stack
+ * with the top element of the stack and removes the top
+ * element to leave the result in a new top
+ * @stack: the stack as a doubly linked list
+ * @line_number: the line number
+ * Return: nothing
  */
-typedef struct stack_s
+void mul_top(stack_t **stack, unsigned int line_number)
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
-} stack_t;
+        stack_t *mul = *stack;
+        size_t len = stack_len(*stack);
 
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
- */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
-extern int node_int;
-void free_stack(stack_t *head);
-void parse_line(char *line, unsigned int line_ctr, stack_t **, FILE *);
-void (*get_inst(char *tok))(stack_t **, unsigned int);
-void push_stack(stack_t **, unsigned int);
-void print_stack(stack_t **, unsigned int);
-void print_top(stack_t **, unsigned int);
-void pop_stack(stack_t **, unsigned int);
-void push_error(char *line, unsigned int line_ctr, stack_t **, FILE *);
-size_t stack_len(const stack_t *);
-void swap_top(stack_t **, unsigned int);
-void add_top(stack_t **, unsigned int);
-void nop_stack(stack_t **, unsigned int);
-void sub_top(stack_t **, unsigned int);
-void div_top(stack_t **, unsigned int);
-void mul_top(stack_t **, unsigned int);
-void mod_top(stack_t **, unsigned int);
-void pchar_top(stack_t **, unsigned int);
-void pstr_stack(stack_t **, unsigned int);
-
-#endif /*MONTY_H*/
+        if (len < 2)
+        {
+                dprintf(STDERR_FILENO, "L%u: can't mul, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        mul = mul->next;
+        mul->n *= (*stack)->n;
+        pop_stack(stack, line_number);
+}
